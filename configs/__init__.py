c61537100b0ad92fc718a8649d69e54dbf5a131d
zhangzixu <justzzer@gmail.com>
2023-08-22 13:48:32 +0800

    [新增] 完善基础工具-日志工具类以及告警工具类

---
+++ configs/__init__.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+"""
+@File     : __init__.py.py
+@Project  : mobile_control
+@Time     : 2023/8/22 10:23
+@Author   : Zhang ZiXu
+@Software : PyCharm
+@Desc     :  
+@Last Modify Time          @Version        @Author
+--------------------       --------        -----------
+2023/8/22 10:23            1.0             Zhang ZiXu
+"""
